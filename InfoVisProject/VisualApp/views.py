6522000f25674598935cc44947c008cac22de665
Chermaraj <mchermaraj@gmail.com>
2019-07-17 13:37:14 -0600

    Commit

--- InfoVisProject/VisualApp/views.py
+++ InfoVisProject/VisualApp/views.py
@@ -22,29 +22,3 @@ def home(request):
         request,
         'app/index.html',context
     )
-
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
-
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
